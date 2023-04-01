8608469aa3376cf3cebba7522f97f1b869ba8f3c
Blake <102875455+blake502@users.noreply.github.com>
2023-04-01 02:43:38 -0400

    Improved Windows build script

--- utils/build.bat
+++ utils/build.bat
@@ -2,14 +2,19 @@
 
 echo Building...
 
-set include_flags=-I%Vulkan_SDK%\include
+set include_flags=-I%vulkan_sdk%\include
 set linker_flags=-luser32 -lwinmm
 set output_dir=bin\
 set output_name=main
 set output_type=.exe
+set output_path=%output_dir%%output_name%%output_type%
 
 if not exist %output_dir% mkdir %output_dir%
 
-clang src\*.c %linker_flags% %include_flags% -o%output_dir%%output_name%%output_type%
+del /Q %output_dir%\*
+
+clang src\*.c %linker_flags% %include_flags% -o%output_path%
+
+if %errorlevel% neq 0 (echo Build failed! [Error: %errorlevel%] && exit)
 
 echo Building complete!
