bd7f0e8a83c6e45e3299b77234007ec0f945d004
Blake <blake@blake.uno>
2023-07-25 22:07:52 -0400

    Commit

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include "defines.h"
 #include "core/assertion.h"
+#include "core/logging.h"
 #include "platform/platform.h"
 
 
 int main()
 {
+    S_INFO("Starting");
     platform_state state;
     S_ASSERT_MSG(platform_startup(&state, "Test", 50, 50, 1280, 720), "Failed to start application");
 
