0b56f15735876ecf6b6287c51cef488b6c3845f1
blake502 <102875455+blake502@users.noreply.github.com>
2023-08-11 10:17:18 -0400

    Clock hooked up

--- engine/src/core/application.c
+++ engine/src/core/application.c
@@ -3,10 +3,13 @@
 #include "game_types.h"
 #include "assertion.h"
 #include "logging.h"
+
 #include "platform/platform.h"
+
 #include "core/event.h"
 #include "core/s_memory.h"
 #include "core/input.h"
+#include "clock.h"
 
 typedef struct application_state {
     game* game_inst;
@@ -15,6 +18,7 @@ typedef struct application_state {
     platform_state platform;
     i16 width;
     i16 height;
+    clock clock;
     f64 last_time;
 } application_state;
 
@@ -72,6 +76,13 @@ b8 application_create(game* game_inst)
 
 b8 application_run()
 {
+    clock_start(&app_state.clock);
+    clock_update(&app_state.clock);
+    app_state.last_time = app_state.clock.elapsed;
+    f64 running_time = 0;
+    u8 frame_count = 0;
+    f64 target_frame_seconds = 1.0f / 60;
+
     S_INFO(get_memory_usage_str());
 
     event_register(EVENT_CODE_APPLICATION_QUIT, 0, application_on_event);
@@ -87,23 +98,45 @@ b8 application_run()
         }
         if(!app_state.is_suspended)
         {
-            //TODO: Delta time
-            if(!app_state.game_inst->update(app_state.game_inst, (f32)0))
+            clock_update(&app_state.clock);
+            f64 current_time = app_state.clock.elapsed;
+            f64 delta = (current_time - app_state.last_time);
+            f64 fram_start_time = platform_get_absolute_time();
+
+            if(!app_state.game_inst->update(app_state.game_inst, (f32)delta))
             {
                 S_FATAL("Game update failed, shutting down!");
                 app_state.is_running = false;
                 break;
             }
 
-            if(!app_state.game_inst->render(app_state.game_inst, (f32)0))
+            if(!app_state.game_inst->render(app_state.game_inst, (f32)delta))
             {
                 S_FATAL("Game render failed, shutting down!");
                 app_state.is_running = false;
                 break;
             }
 
-            //TODO: Detla time
-            input_update(0);
+            f64 frame_end_time = platform_get_absolute_time();
+            f64 frame_elapsed_time = frame_end_time - fram_start_time;
+            running_time += frame_elapsed_time;
+            f64 remaining_seconds = target_frame_seconds - frame_elapsed_time;
+            
+            if(remaining_seconds > 0)
+            {
+                u64 remaining_ms = (remaining_seconds * 1000);
+
+                //TODO: Enable sleeping
+                b8 limit_frames = false;
+                if (remaining_ms > 0 && limit_frames)
+                    platform_sleep(remaining_ms - 1);
+
+                frame_count++;
+            }
+
+            input_update(delta);
+
+            app_state.last_time = current_time;
         }
     }
 
